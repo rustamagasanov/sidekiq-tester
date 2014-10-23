9ff60e87fb2e64048254bdf2ba5dc2c2c5d90cb2
Rustam Gasanov <rustamagasanov@gmail.com>
2014-10-23 19:57:47 +0400

    [+] added redis-server check on initialize

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,11 @@
 schedule_file = 'config/schedule.yml'
 
-if File.exists?(schedule_file)
+begin
+  redis = Sidekiq.redis { |conn| conn.info }
+rescue Redis::CannotConnectError
+  redis = nil
+end
+
+if File.exists?(schedule_file) && redis.present?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
